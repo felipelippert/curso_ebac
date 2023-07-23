c07a152124fbe3ef4e4f0d0dd94f4471de217127
felipelippert <felipe.v.lippert@gmail.com>
2023-07-23 18:31:55 -0300

    finished exercise

--- exercicio_jquery/main.js
+++ exercicio_jquery/main.js
@@ -1,13 +1,34 @@
 $(document).ready(function() {
+    const tarefas = []
+    const campo = $('form input')
+    const spanSucesso = '<span class="span-sucesso">Atividade adicionada com sucesso</span>'
+    const spanErro = '<span class="span-erro">Essa tarefa já foi adicionada.</span>'
 
-    
-
-    $('form').on('submit', function (e) {
+        $('form').on('submit', function (e) {
         e.preventDefault()
 
-        const campo = $('form input').val()
-        const novoItem = $(`<li>${campo}</li>`)
-        $(novoItem).appendTo('ul')
+        if (tarefas.includes(campo.val())) {
+            $(spanErro).appendTo('p')
+            
+        }
+        else {
+            
+            tarefas.push(campo.val())
+            const novoItem = $(`<li id="item">${campo.val()}</li>`)
+            $(novoItem).appendTo('ul')
+            $(spanSucesso).appendTo('p')
+        
+        }
+        $('form input').val('')
+        console.log(tarefas)
+
+        $('button').on('click', function() {
+            $('span').css('display', 'none')
+        })
+    })
+    $('ul').on('click', function() {
+        $('li').css('text-decoration', 'line-through')
         
     })
+    
 })
