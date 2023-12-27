6453b490a927914287df30629da544c7e1b40cff
Felipe Valcarenghi Lippert <felipe.v.lippert@gmail.com>
2023-12-27 13:53:46 -0300

    js update

---
+++ AJAX/main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('btn-search').addEventListener('click', function () {
+        const xhhtp = new XMLHttpRequest();
+        const zip = document.getElementById('zip')
+
+    // https://viacep.com.br/ws/231231231/json
+    })
+})
