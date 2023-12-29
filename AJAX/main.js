1ff86d76997873733fc3b31d58eb82dbebd77966
unknown <felipe.v.lippert@gmail.com>
2023-12-28 23:30:42 -0300

    ajax

--- AJAX/main.js
+++ AJAX/main.js
@@ -1,8 +1,29 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('btn-search').addEventListener('click', function () {
-        const xhhtp = new XMLHttpRequest();
-        const zip = document.getElementById('zip')
+$(document).ready(function() {
+    $('#zip').mask('00000-000')
 
-    // https://viacep.com.br/ws/231231231/json
+
+    $('#btn-search').click(function() {
+        const zip = $('#zip').val();
+        const endpoint = `https://viacep.com.br/ws/${zip}/json`;
+        const button = $(this);
+        $(button).find('i').addClass('d-none')
+        $(button).find('span').removeClass('d-none')
+
+        $.ajax(endpoint).done(function(response) {
+            const rua = response.logradouro;
+            const bairro = response.bairro;
+            const cidade = response.localidade;
+            const estado = response.uf;
+            const address = `${rua}, ${bairro} - ${cidade} - ${estado} `;
+            $('#address').val(address)
+
+
+            setTimeout(function() {
+                $(button).find('i').removeClass('d-none')
+                $(button).find('span').addClass('d-none')
+                
+            }, 3000);
+
+        })
     })
 })
