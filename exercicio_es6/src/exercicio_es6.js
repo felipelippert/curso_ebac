d9623dea5337de425fee3deb6a66bd4128895e04
Felipe Valcarenghi Lippert <felipe.v.lippert@gmail.com>
2024-01-19 18:10:27 -0300

    Update exercicio_es6.js

--- exercicio_es6/src/exercicio_es6.js
+++ exercicio_es6/src/exercicio_es6.js
@@ -9,13 +9,11 @@ const student1 =  new Student("Vanessa", 7);
 const student2 = new Student("John", 5);
 const student3 = new Student("Felipe", 6);
 const student4 = new Student("Sabrina", 9);
-const student5 = new Student("Andrea", 3);
+const student5 = new Student("Andrea", 3);2
 
 students.push(student1, student2, student3, student4, student5);
 
-console.log('The following students were approved:')
-for (let i = 0; i < students.length; i++) {
-    if (students[i].grade >= 6) {
-        console.log(students[i]);
-    }
-}
+
+const approved = students.filter(student => (student.grade >= 6));
+
+console.log(approved)
